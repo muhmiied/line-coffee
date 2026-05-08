2548264aa3547f4bc8ebc342ef7599cde0131dde
Mohamed Sayed <midoseka8@gmail.com>
2026-05-08 19:38:23 +0300

    fix: wrap not-found in suspense

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,17 @@
-export default function NotFound() {
+import { Suspense } from 'react'
+
+function NotFoundContent() {
   return (
     <div>
       <h2>Page Not Found</h2>
     </div>
-  );
+  )
+}
+
+export default function NotFound() {
+  return (
+    <Suspense>
+      <NotFoundContent />
+    </Suspense>
+  )
 }
